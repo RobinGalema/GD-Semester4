42f08eaa5a79eb608165c59609fb26733e62352d
Robin <Robin99g@gmail.com>
2020-03-03 13:24:29 +0100

    Added reset and player prefab

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -9,14 +9,17 @@ public class playerMovement : MonoBehaviour
     public float checkRadius;
     public LayerMask whatIsGround;
     public float jumpForce;
+    public Transform fallDetector;
 
     private Rigidbody2D rb;
     private float horizontalMovement = 0f;
     private bool isGrounded;
+    private Vector2 spawnPos;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spawnPos = rb.position;
     }
 
     // Update is called once per frame
@@ -25,9 +28,17 @@ public class playerMovement : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         horizontalMovement = Input.GetAxisRaw("Horizontal");
 
+        // Check jump input
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            Debug.Log("Space pressed0");
             rb.velocity = Vector2.up * jumpForce;
+        }
+
+        // Check if the player fell out of the level
+        if (rb.position.y < fallDetector.position.y)
+        {
+            resetPlayer();
         }
     }
 
@@ -35,4 +46,12 @@ public class playerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(horizontalMovement * movespeed, rb.velocity.y);
     }
+
+    private void resetPlayer()
+    {
+        // Player gets reset
+        Debug.Log("--- Resetting player to spawning position ---");
+        rb.position = spawnPos;
+    }
 }
+
