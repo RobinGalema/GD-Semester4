1f98277d17cfed2d05291e030f5394dc6e050a28
Thomas <393890@student.fontys.nl>
2020-03-12 13:21:57 +0100

    Spikes working
    get reset to origin on collision

---
+++ Assets/Scripts/playerInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInteraction : MonoBehaviour
+{
+    playerMovement playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<playerMovement>();
+    }
+
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Spike")
+        {
+            playerController.resetPlayer();
+        }
+    }
+}
