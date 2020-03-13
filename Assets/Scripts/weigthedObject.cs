fecdd9fd03e5e416b8f092480b33c759a0b0d181
Robin <Robin99g@gmail.com>
2020-03-13 12:59:23 +0100

    Basic box to push around

---
+++ Assets/Scripts/weigthedObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class weigthedObject : MonoBehaviour
+{
+    private bool playerInRange = false;
+    public bool yeet;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("--- Player in box range [" + this.name + "] ---");
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("--- Player leaving box range [" + this.name + "] ---");
+            playerInRange = false;
+        }
+    }
+
+}
