2cfdd65acf61ed3f1acb5dfe030567ca153bec11
Thomas <393890@student.fontys.nl>
2020-03-27 13:44:18 +0100

    jump, idle animations added

---
+++ Assets/animationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private playerMovement movement;
+    private Animator animator;
+    void Start()
+    {
+        movement = GetComponent<playerMovement>();
+        animator = GetComponentInChildren<Animator>();
+        animator.Play("walking");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
