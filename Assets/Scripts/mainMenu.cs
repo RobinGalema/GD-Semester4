2075e770b13cf76b63caa6a426499405c0d73ffd
Robin <Robin99g@gmail.com>
2020-04-19 15:00:18 +0200

    some final adjustments for building

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -6,10 +6,19 @@ using UnityEngine.SceneManagement;
 public class mainMenu : MonoBehaviour
 {
 
+    public PlayerInfo player1Info;
+    public PlayerInfo player2Info;
+
     public void PlayGame()
     {
 
         Debug.Log("button start clicked");
+
+        player1Info.spawnPos = Vector2.zero;
+        player2Info.spawnPos = new Vector2(-5.5f, -5.5f);
+        player1Info.StoreToDisk();
+        player2Info.StoreToDisk();
+
         SceneManager.LoadScene("IntroLevel");
     }
 
